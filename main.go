cba4394bbb72164030eda4a9ee3e2aac282d8a75
Charles Wang <charles@thredup.com>
2014-11-15 18:37:10 -0800

    a simple markdown generator

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/russross/blackfriday"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/markdown", GenerateMarkdown)
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.ListenAndServe(":"+port, nil)
+}
+
+func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
+	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
+	rw.Write(markdown)
+}
